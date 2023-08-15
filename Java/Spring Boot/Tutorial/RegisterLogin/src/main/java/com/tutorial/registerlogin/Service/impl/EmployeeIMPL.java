3d865186abdffb12b734e03e691ba9cc927673d8
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-08-15 14:33:25 +0700

    Create EmployeeIMPL.java

---
+++ Java/Spring Boot/Tutorial/RegisterLogin/src/main/java/com/tutorial/registerlogin/Service/impl/EmployeeIMPL.java
@@ -0,0 +1,14 @@
+package com.tutorial.registerlogin.Service.impl;
+
+import com.tutorial.registerlogin.Dto.EmployeeDTO;
+import com.tutorial.registerlogin.Service.EmployeeService;
+
+public class EmployeeIMPL implements EmployeeService{
+
+	@Override
+	public String addEmployee(EmployeeDTO employeeDTO) {
+		
+		return null;
+	}
+
+}
