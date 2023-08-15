3af2d4b846c63ca44c76af61a0cd08254bea48f4
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-08-15 14:39:44 +0700

    Update EmployeeIMPL.java

--- Java/Spring Boot/Tutorial/RegisterLogin/src/main/java/com/tutorial/registerlogin/Service/impl/EmployeeIMPL.java
+++ Java/Spring Boot/Tutorial/RegisterLogin/src/main/java/com/tutorial/registerlogin/Service/impl/EmployeeIMPL.java
@@ -1,14 +1,34 @@
 package com.tutorial.registerlogin.Service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.tutorial.registerlogin.Dto.EmployeeDTO;
+import com.tutorial.registerlogin.Entity.Employee;
+import com.tutorial.registerlogin.Repo.EmployeeRepo;
 import com.tutorial.registerlogin.Service.EmployeeService;
 
 public class EmployeeIMPL implements EmployeeService{
 
+	@Autowired
+    private EmployeeRepo employeeRepo;
+	
+	@Autowired
+    private PasswordEncoder passwordEncoder;
+	
 	@Override
 	public String addEmployee(EmployeeDTO employeeDTO) {
 		
-		return null;
+	 	Employee employee = new Employee(
+	        employeeDTO.getEmployeeid(),
+	        employeeDTO.getEmployeename(),
+	        employeeDTO.getEmail(),
+	        this.passwordEncoder.encode(employeeDTO.getPassword())
+		);
+	 	
+		employeeRepo.save(employee);
+		
+		return employee.getEmployeename();
 	}
 
 }
