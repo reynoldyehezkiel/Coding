e59c41588a67db2deda7e28206911b03c538facb
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-08-15 14:31:41 +0700

    Create EmployeeService.java

---
+++ Java/Spring Boot/Tutorial/RegisterLogin/src/main/java/com/tutorial/registerlogin/Service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.tutorial.registerlogin.Service;
+
+import com.tutorial.registerlogin.Dto.EmployeeDTO;
+
+public interface EmployeeService {
+
+	String addEmployee(EmployeeDTO employeeDTO);
+
+}
