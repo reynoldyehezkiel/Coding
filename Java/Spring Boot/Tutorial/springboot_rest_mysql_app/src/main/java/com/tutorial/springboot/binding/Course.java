40369b64ce6aa22466b7f34d9bc0e7b080556e6e
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-08-15 10:44:30 +0700

    Add Getter Setter

--- Java/Spring Boot/Tutorial/springboot_rest_mysql_app/src/main/java/com/tutorial/springboot/binding/Course.java
+++ Java/Spring Boot/Tutorial/springboot_rest_mysql_app/src/main/java/com/tutorial/springboot/binding/Course.java
@@ -19,4 +19,27 @@ public class Course {
 	private String name;
 	private Double price;
 
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 }
