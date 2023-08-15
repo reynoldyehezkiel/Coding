5be584ff37f8b9f638794a031bc2a8d31073b5fb
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-08-15 14:31:49 +0700

    Update EmployeeController.java

--- Java/Spring Boot/Tutorial/RegisterLogin/src/main/java/com/tutorial/registerlogin/EmployeeController/EmployeeController.java
+++ Java/Spring Boot/Tutorial/RegisterLogin/src/main/java/com/tutorial/registerlogin/EmployeeController/EmployeeController.java
@@ -1,16 +1,23 @@
 package com.tutorial.registerlogin.EmployeeController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tutorial.registerlogin.Dto.EmployeeDTO;
+import com.tutorial.registerlogin.Service.EmployeeService;
+
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/employee")
 public class EmployeeController {
 
+	@Autowired
+	private EmployeeService employeeService;
+	
 	@PostMapping(path = "/save")
     public String saveEmployee(@RequestBody EmployeeDTO employeeDTO)
     {
